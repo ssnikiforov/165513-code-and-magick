8fe104eb4a8e115bbacddf34476605a66fa9c0d3
ssnikiforov <stepan001@yandex.ru>
2018-02-19 13:09:15 +0300

    Handle drag&drop artifacts

--- js/dialog-drag-and-drop.js
+++ js/dialog-drag-and-drop.js
@@ -2,6 +2,7 @@
 
 (function () {
 
+  // drag and drop userDialog
   var userDialog = window.userDialog;
   var dialogHandler = userDialog.querySelector('.upload');
 
@@ -52,4 +53,38 @@
     document.addEventListener('mouseup', onMouseUp);
   });
 
+  // drag and drop artifacts
+  var shopElement = document.querySelector('.setup-artifacts-shop');
+  var draggedItem = null;
+
+  shopElement.addEventListener('dragstart', function (evt) {
+    if (evt.target.tagName.toLowerCase() === 'img') {
+      draggedItem = evt.target;
+      evt.dataTransfer.setData('text/plain', evt.target.alt);
+    }
+  });
+
+  var artifactsElement = document.querySelector('.setup-artifacts');
+
+  artifactsElement.addEventListener('dragover', function (evt) {
+    evt.preventDefault();
+    return false;
+  });
+
+  artifactsElement.addEventListener('drop', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.target.appendChild(draggedItem);
+    evt.preventDefault();
+  });
+
+
+  artifactsElement.addEventListener('dragenter', function (evt) {
+    evt.target.style.backgroundColor = 'yellow';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragleave', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.preventDefault();
+  });
 })();
